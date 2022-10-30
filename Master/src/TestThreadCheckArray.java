83137ebf5fbde5c91aba8d70468386ca989a4280
elroe <elroe@10.10.13.88>
2022-10-30 13:11:34 +0200

    hhhh

--- Master/src/TestThreadCheckArray.java
+++ Master/src/TestThreadCheckArray.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 ///
-
+///
 public class TestThreadCheckArray {
 	public static void main(String[] args) {
 		try (Scanner input = new Scanner(System.in)) {
